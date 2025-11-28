7a1a77a6c8797d3d9041a74292f3e6992b75c9c7
sunday <iamdyclef@gmail.com>
2025-11-28 18:51:32 +0100

    feat: new domain

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:5173", // dev
-  "glams-hair.vercel.app", // your Vercel frontend origin (exact)
+  "https://glams-hair.vercel.app", // your Vercel frontend origin (exact)
 ];
 
 // ---------- Middleware ----------
